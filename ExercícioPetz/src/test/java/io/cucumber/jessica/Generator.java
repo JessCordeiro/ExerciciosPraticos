286b66d8abd1ac306b7d7d08b92e50b05cd78656
JessCordeiro <67651751+JessCordeiro@users.noreply.github.com>
2021-04-23 09:23:37 -0300

    Exercício automação Petz

---
+++ ExercícioPetz/src/test/java/io/cucumber/jessica/Generator.java
@@ -0,0 +1,13 @@
+package io.cucumber.jessica;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+
+public class Generator {
+
+	      public static String dataHoraParaArquivo(){
+	            Timestamp ts = new Timestamp(System.currentTimeMillis());
+	            return new SimpleDateFormat("yyyyMMddhhmmss").format(ts); 
+	}
+}
